b2f59009b191e1e8ecf3c17d951d1a20303b7106
kuldeeptyagi83 <kuldeeptyagi83>
2025-09-27 04:03:52 +0000

    rearranged folder and created new dlt folder

--- formula1/utils/utils.py
+++ formula1/utils/utils.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp
 # need to do this in .py file but auto avaiable in notebook file
 # Create or get existing Spark session
 spark = SparkSession.builder.getOrCreate()
@@ -27,3 +28,15 @@ def load_and_register_csv_as_table(csv_path, catalog, schema, table):
     spark.sql(f"DROP TABLE IF EXISTS {full_table_name}")
     df.write.saveAsTable(full_table_name)
     return df
+
+def add_ingestion_date(df,column_name="default_ingestion_date_arg"	):
+    """
+    Adds an ingestion date column to the DataFrame.
+
+    Parameters:
+        df (DataFrame): The DataFrame to add the column to.
+
+    Returns:
+        DataFrame: The DataFrame with the added column.
+    """
+    return df.withColumn(column_name, current_timestamp())
